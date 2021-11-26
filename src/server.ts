07981ddb9b75c1d28f420df8c95464b23ca78de7
Gabriel Moraes <gabrielhm2001@gmail.com>
2021-11-26 19:27:26 -0300

    chore: add typeorm

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import swaggerUi from 'swagger-ui-express'
 import swaggerDocument from './swagger.json'
 
+import '@/database'
+
 const app = express()
 app.use(express.json())
 
