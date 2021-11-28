a7a613e4d94138bcba50be249dc669ed0bc05a45
Gabriel Moraes <gabrielhm2001@gmail.com>
2021-11-28 01:26:01 -0300

    chore: add User entity

--- src/modules/accounts/entities/User.ts
+++ src/modules/accounts/entities/User.ts
@@ -1,5 +1,25 @@
-import {} from 'uuid'
+import { v4 as uuidV4 } from 'uuid'
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
 
-class User {}
+@Entity('users')
+class User {
+  @PrimaryColumn()
+  id: string
+
+  @Column()
+  email: string
+
+  @Column()
+  password: string
+
+  @CreateDateColumn()
+  created_at: Date
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4()
+    }
+  }
+}
 
 export { User }
