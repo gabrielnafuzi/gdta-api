76a5caf3ef1162523bd34e2f43a98674e3d6252b
Gabriel Moraes <gabrielhm2001@gmail.com>
2021-12-02 22:49:17 -0300

    feat: add validate token route

--- src/routes/authenticate.routes.ts
+++ src/routes/authenticate.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 
+import { ensureAuthenticated } from '@/middleware'
 import { AuthenticateUserController } from '@/modules/accounts/use-cases/authenticateUser/AuthenticateUserController'
 
 const authenticateRoutes = Router()
@@ -7,5 +8,10 @@ const authenticateRoutes = Router()
 const authenticateUserController = new AuthenticateUserController()
 
 authenticateRoutes.post('/sign-in', authenticateUserController.handle)
+authenticateRoutes.post('/validate-token', ensureAuthenticated, (req, res) => {
+  return res.status(200).json({
+    message: 'Token is valid'
+  })
+})
 
 export { authenticateRoutes }
